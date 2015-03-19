cbe23f18463f1cf46e1059a2b583afd862e9c046
Stephen Jennings <stephen.g.jennings@gmail.com>
2015-03-18 17:27:56 -0700

    Add documentation to Consumer/Producer interfaces

--- src/Turbocharged.Beanstalk/IConsumer.cs
+++ src/Turbocharged.Beanstalk/IConsumer.cs
@@ -8,10 +8,36 @@ namespace Turbocharged.Beanstalk
 {
     public interface IConsumer
     {
+        /// <summary>
+        /// Reserve a job, waiting indefinitely.
+        /// </summary>
+        /// <returns>A reserved job.</returns>
         Task<JobDescription> ReserveAsync();
+
+        /// <summary>
+        /// Reserve a job, waiting for the specified timeout.
+        /// </summary>
+        /// <param name="timeout"></param>
+        /// <returns>A reserved job, or null if the timeout elapses.</returns>
         Task<JobDescription> ReserveAsync(TimeSpan timeout);
+
+        /// <summary>
+        /// Begins watching a tube.
+        /// </summary>
+        /// <returns>The number of tubes currently being watched.</returns>
         Task<int> Watch(string tube);
+
+        /// <summary>
+        /// Ignores jobs from a tube.
+        /// </summary>
+        /// <returns>The number of tubes currently being watched.</returns>
+        /// <exception cref="System.InvalidOperationException">Thrown when attempting to ignore the only watched tube.</exception>
         Task<int> Ignore(string tube);
+
+        /// <summary>
+        /// Retrives a job without reserving it.
+        /// </summary>
+        /// <returns>A job, or null if the job was not found.</returns>
         Task<JobDescription> PeekAsync(int id);
     }
 }
--- src/Turbocharged.Beanstalk/IProducer.cs
+++ src/Turbocharged.Beanstalk/IProducer.cs
@@ -8,10 +8,38 @@ namespace Turbocharged.Beanstalk
 {
     public interface IProducer
     {
+        /// <summary>
+        /// Uses the specified tube. Jobs will be inserted into the currently-used tube.
+        /// </summary>
+        /// <returns></returns>
         Task<string> Use(string tube);
+
+        /// <summary>
+        /// Puts a new job into the currently-used tube.
+        /// </summary>
+        /// <param name="job">The job data.</param>
+        /// <param name="priority">The priority of the job. Higher-priority jobs will be delivered before lower-priority jobs.</param>
+        /// <param name="delay">The number of seconds the server should wait before allowing the job to be reserved.</param>
+        /// <param name="ttr">The number of seconds for which this job will be reserved.</param>
+        /// <returns>The ID of the inserted job.</returns>
         Task<int> PutAsync(byte[] job, int priority, int delay, int ttr);
+
+        /// <summary>
+        /// Retrives a job without reserving it.
+        /// </summary>
+        /// <returns>A job, or null if the job was not found.</returns>
         Task<JobDescription> PeekAsync(int id);
+
+        /// <summary>
+        /// Retrives the first job from the Ready state in the currently-used tube.
+        /// </summary>
+        /// <returns>A job, or null if there are no jobs in the Ready state.</returns>
         Task<JobDescription> PeekAsync();
+
+        /// <summary>
+        /// Retrives the first job from the specified state in the currently-used tube.
+        /// </summary>
+        /// <returns>A job, or null if no jobs are in the specified state.</returns>
         Task<JobDescription> PeekAsync(JobStatus status);
     }
 }
