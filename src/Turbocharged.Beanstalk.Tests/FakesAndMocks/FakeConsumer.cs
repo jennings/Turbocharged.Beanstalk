ec8f29411d3b3382543dfce43e5a35e9636e12c8
Stephen Jennings <stephen.g.jennings@gmail.com>
2016-01-19 21:31:11 -0800

    Move fakes and mocks into a folder

--- src/Turbocharged.Beanstalk.Tests/FakesAndMocks/FakeConsumer.cs
+++ src/Turbocharged.Beanstalk.Tests/FakesAndMocks/FakeConsumer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Turbocharged.Beanstalk.Tests
+namespace Turbocharged.Beanstalk.Tests.FakesAndMocks
 {
     class FakeConsumer : IConsumer
     {
--- src/Turbocharged.Beanstalk.Tests/SerializationFacts.cs
+++ src/Turbocharged.Beanstalk.Tests/SerializationFacts.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+using Turbocharged.Beanstalk.Tests.FakesAndMocks;
 using Xunit;
 
 namespace Turbocharged.Beanstalk.Tests
@@ -168,25 +168,5 @@ namespace Turbocharged.Beanstalk.Tests
     {
         public int Int { get; set; }
         public string String { get; set; }
-    }
-
-    class CountingSerializer : IJobSerializer
-    {
-        public int SerializeCount = 0;
-        public int DeserializeCount = 0;
-
-        public byte[] Serialize<T>(T job)
-        {
-            SerializeCount++;
-            var str = JsonConvert.SerializeObject(job, Formatting.None);
-            return Encoding.UTF8.GetBytes(str);
-        }
-
-        public T Deserialize<T>(byte[] buffer)
-        {
-            DeserializeCount++;
-            var str = Encoding.UTF8.GetString(buffer);
-            return JsonConvert.DeserializeObject<T>(str);
-        }
     }
 }
